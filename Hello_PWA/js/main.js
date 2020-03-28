015ac3978781102bd02fde607541b4c3b08b185c
Bavyasri Iyengar <56211217+bavyasriyengar@users.noreply.github.com>
2020-03-28 22:00:30 +0530

    Add files via upload

---
+++ Hello_PWA/js/main.js
@@ -0,0 +1,14 @@
+window.onload = () => {  
+    'use strict';     
+    if ('serviceWorker' in navigator){
+        navigator.serviceWorker.register('/sw.js').then(function(registration){
+          console.log('service worker registration succeeded:',registration);
+        },
+      function(error){
+        console.log('service worker registration failed:',error);
+      });
+      }
+      else{
+        console.log('service workers are not supported.');
+      }
+    }
